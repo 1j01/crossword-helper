1j01/crossword-helper

commit 523d24ce6df400cda70be454c6d437def34171cb

Use `words_by_length`

src/dictionary.py
@@ -13,7 +13,7 @@ import sys
 # worddb.json structure: { words: {[length]: [[word, score], ...], ...} }
 with open('data/worddb.json', 'r', encoding='utf-8') as f:
     worddb = json.load(f)
-    words_by_length = {}
+    words_by_length: dict[int, list[str]] = {}
     for length, words_of_length in worddb['words'].items():
         words_by_length[length] = [word for word, score in words_of_length]
     words = [word for sublist in words_by_length.values() for word in sublist]

src/superpuzzition.py
@@ -3,16 +3,14 @@ from collections import defaultdict
 import sys
 from numpy import ndarray
 
-# TODO: use words_by_length
-from .dictionary import words
+from .dictionary import words_by_length
 
 
 def find_superpuzzitions(target_length: int, target_letters: list[str], target_position: int | None = None, exactly_one_different=False) -> list[tuple[str, str, float]]:
 	# Match dictionary letter case
 	target_letters = [letter.upper() for letter in target_letters]
 
-	words_fitting_length = [word for word in words if len(word) == target_length]
-	if not words_fitting_length:
+	if target_length not in words_by_length:
 		raise ValueError(f"No words in dictionary with length {target_length}")
 
 	if exactly_one_different:
@@ -26,7 +24,7 @@ def find_superpuzzitions(target_length: int, target_letters: list[str], target_p
 
 	for target_position in ([target_position] if target_position is not None else range(target_length)):
 		words_by_letter_there: defaultdict[str, list[str]] = defaultdict(list)
-		for word in words_fitting_length:
+		for word in words_by_length[target_length]:
 			if target_position < len(word) and word[target_position] in target_letters:
 				words_by_letter_there[word[target_position]].append(word)
 

commit 7a0b0ee02a061fa20af976d879ccbb766705e587

Fix `words_by_length` key type
Fixes superpuzzition command broken in 523d24ce6df400cda70be454c6d437def34171cb
It was outputting "ValueError: No words in dictionary with length 5"
when there are obviously words of length 5 in the dictionary.

src/dictionary.py
@@ -15,7 +15,7 @@ with open('data/worddb.json', 'r', encoding='utf-8') as f:
     worddb = json.load(f)
     words_by_length: dict[int, list[str]] = {}
     for length, words_of_length in worddb['words'].items():
-        words_by_length[length] = [word for word, score in words_of_length]
+        words_by_length[int(length)] = [word for word, score in words_of_length]
     words = [word for sublist in words_by_length.values() for word in sublist]
 
 print(f"Loaded {len(words)} words from worddb.json", file=sys.stderr)