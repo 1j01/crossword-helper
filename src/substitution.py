100b885ce203300274cda531d075e5116f5583cd
Isaiah Odhner <isaiahodhner@gmail.com>
2026-02-10 11:22:26 -0500

    Try all subsets of possible substitutions

--- src/substitution.py
+++ src/substitution.py
@@ -1,11 +1,16 @@
 
-from collections import defaultdict
-import itertools
-import logging
 from typing import NamedTuple
-import re
+from itertools import chain, combinations
 
-# from torch import Tensor
+def powerset(iterable):
+    """
+	Returns all possible subsets of the input iterable,
+	including the empty set and the full set of values from the iterable.
+
+    powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+	"""
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 class SubstitutionalResult(NamedTuple):
 	words: tuple[str, ...]
@@ -35,12 +40,26 @@ def find_substitutions(
 				occurances = (i for i in range(len(word) - len(base_seq) + 1)
 				  if word[i:i+len(base_seq)] == base_seq
 				)
-				# TODO: handle multiple substitutions in one word [optionally]
-				for i in occurances:
-					prefix = word[:i]
-					suffix = word[i+len(base_seq):]
+				# handle multiple substitutions in one word [TODO: optionally]
+				for substitution_points in powerset(occurances):
+					if not substitution_points:
+						continue
+					# Check for overlapping substitutions
+					# This assumes substitution_points is ordered for efficiency.
+					overlaps = False
+					for i in range(len(substitution_points) - 1):
+						if substitution_points[i] + len(base_seq) > substitution_points[i+1]:
+							overlaps = True
+							break
+					if overlaps:
+						continue
 
-					other_words_that_must_exist = [prefix + seq + suffix for seq in letter_sequences[1:]]
+					other_words_that_must_exist = []
+					for seq in letter_sequences[1:]:
+						new_word = word
+						for point in substitution_points:
+							new_word = new_word[:point] + seq + new_word[point+len(base_seq):]
+						other_words_that_must_exist.append(new_word)
 					if all(other_word in all_words_set for other_word in other_words_that_must_exist):
 						results.append(SubstitutionalResult((word, *other_words_that_must_exist)))
 
